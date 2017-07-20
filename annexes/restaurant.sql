da437a7d2b0525a48c7ef54e5d2713006791b3cf
FerEmilie <fernandezemilie@ymail.com>
2017-07-20 15:58:23 +0200

    ajout sql

--- annexes/restaurant.sql
+++ annexes/restaurant.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Mar 18 Juillet 2017 à 16:46
+-- Généré le :  Jeu 20 Juillet 2017 à 15:57
 -- Version du serveur :  5.5.55-0+deb8u1
 -- Version de PHP :  5.6.30-0+deb8u1
 
@@ -50,16 +50,17 @@ CREATE TABLE IF NOT EXISTS `menu` (
 `id` int(11) NOT NULL,
   `nom` varchar(255) NOT NULL,
   `prix` float NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `menu`
 --
 
 INSERT INTO `menu` (`id`, `nom`, `prix`) VALUES
-(2, 'Enfant', 12),
+(2, 'Enfant', 15.5),
 (6, 'Marché', 34),
-(7, 'Dégustation', 56);
+(7, 'Dégustation', 46),
+(8, 'Végé', 30);
 
 -- --------------------------------------------------------
 
@@ -71,23 +72,26 @@ CREATE TABLE IF NOT EXISTS `menu_plat` (
 `id` int(11) NOT NULL,
   `id_menu` int(11) NOT NULL,
   `id_plat` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=111 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=110 DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `menu_plat`
 --
 
 INSERT INTO `menu_plat` (`id`, `id_menu`, `id_plat`) VALUES
-(83, 2, 7),
-(84, 2, 9),
 (85, 6, 5),
 (86, 6, 11),
 (87, 6, 14),
-(94, 7, 1),
-(95, 7, 3),
-(96, 7, 5),
-(97, 7, 11),
-(98, 7, 15);
+(98, 8, 11),
+(99, 8, 5),
+(100, 8, 9),
+(101, 2, 7),
+(102, 2, 8),
+(103, 7, 3),
+(104, 7, 11),
+(105, 7, 1),
+(106, 7, 5),
+(107, 7, 16);
 
 -- --------------------------------------------------------
 
@@ -101,7 +105,7 @@ CREATE TABLE IF NOT EXISTS `plat` (
   `id_type_plat` int(11) NOT NULL,
   `prix` float NOT NULL,
   `image` varchar(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `plat`
@@ -117,7 +121,8 @@ INSERT INTO `plat` (`id`, `nom_plat`, `id_type_plat`, `prix`, `image`) VALUES
 (11, 'Velouté d''été', 1, 16.5, 'veloute.JPG'),
 (13, 'BoBun', 1, 16, 'bobun.jpeg'),
 (14, 'Tarte au citron', 3, 9, 'citron.jpg'),
-(15, 'Tiramisu framboise', 3, 12, '');
+(16, 'Tiramisu framboise', 3, 11, 'tiramisu.jpg'),
+(34, 'Sushi', 1, 11, 'sushi.jpeg');
 
 -- --------------------------------------------------------
 
@@ -185,17 +190,17 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 -- AUTO_INCREMENT pour la table `menu`
 --
 ALTER TABLE `menu`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=24;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT pour la table `menu_plat`
 --
 ALTER TABLE `menu_plat`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=111;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=110;
 --
 -- AUTO_INCREMENT pour la table `plat`
 --
 ALTER TABLE `plat`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=16;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=35;
 --
 -- AUTO_INCREMENT pour la table `users`
 --
